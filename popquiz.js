23d24508d58ecbd7ca3ec081d09a058ba7ac89ce
travenell911 <52293704+travenell911@users.noreply.github.com>
2019-06-27 13:28:46 -0400

    Add files via upload

---
+++ popquiz.js
@@ -0,0 +1,20 @@
+(function() {
+  const myQuestions = [
+    {
+      question: "Who is the strongest?",
+      answers: {
+        a: "Superman",
+        b: "The Terminator",
+        c: "Waluigi, obviously"
+      },
+      correctAnswer: "c"
+    },
+    {
+      question: "What is the best site ever created?",
+      answers: {
+        a: "SitePoint",
+        b: "Simple Steps Code",
+        c: "Trick question; they're both the best"
+      },
+      correctAnswer: "c"
+    },
